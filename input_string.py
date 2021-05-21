7355da2450cf948ca6f3c7644f68f4d1a63fad77
ibman77 <mnaufal.ib3@gmail.com>
2021-05-21 10:22:14 +0700

    Project ke 2

---
+++ input_string.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+
+print(f"Hallo {name}")
+
+condition = input("How are you today?: ")
+
+print(f"{condition}")
+
+age = input("How old are you?: ")
+
+print(f"I'm {age} y o")
+
