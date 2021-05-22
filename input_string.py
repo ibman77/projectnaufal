143fec8d765e5b38371047b3ee557df8ea93aa1e
ibman77 <mnaufal.ib3@gmail.com>
2021-05-22 13:53:00 +0700

    Project-4(with an error description)

--- input_string.py
+++ input_string.py
@@ -1,11 +1,24 @@
 print("Menghitung Luas Persegi Panjang")
+print("-------------------------------")
 
-size_input = input("Input panjang: ")
-size_input2 = input("Input lebar: ")
+size1 = input("Input panjang: ")
+try:
+    panjang = int(size1)
+except:
+    print("Error, please use numeric digits.")
+if panjang<1:
+    print("Error, please enter a positive numbers.")
 
-panjang = int(size_input)
-lebar = int(size_input2)
+size2 = input("Input lebar: ")
+try:
+    lebar = int(size2)
+except:
+    print("Error, please use numeric digits.")
+if lebar<1:
+    print("Error, please enter a positive numbers.")
 
 luas = panjang * lebar
 
 print(f"Luas Persegi Panjang = {luas}")
+if luas<1:
+    print("The result should be a positive numbers.")
